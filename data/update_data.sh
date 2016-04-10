0e4d42aa9217938f7286c5007dc086653d02b410
Kevin Stein <kestein42932@gmail.com>
2016-04-09 22:41:19 -0400

    Added files via upload
    Per county results

---
+++ data/update_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl https://int.nyt.com/applications/elections/2016/api/delegates/total.json > total.json
+curl https://int.nyt.com/applications/elections/2016/api/election/primaries.json > primaries.json
